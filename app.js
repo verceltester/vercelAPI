af5703c42deec5eff93821ae212b9283846c95b5
verceltester <paheencodes@gmail.com>
2023-04-30 21:04:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import fetch from "node-fetch"
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api", async (req, res) => {
+  try {
+    const response = await fetch(req.query.url)
+    const data = await response.text()
+    console.log("sending data")
+    console.log(req.query.url)
+    res.send(data)
+  } catch(err) {
+    res.send("There's some error, please check the URL and try again")
+  }
+  // console.log(data)
+})
+
+app.listen(3000, () => {
+  console.log("cacheBackend listening on port 3000")
+})
